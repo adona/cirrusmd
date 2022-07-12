4d84ef5c7b58c8197fa044646338e5559abd8819
Adona Iosif <adona.iosif@gmail.com>
2022-07-12 13:47:14 -0400

    Add Patient class with transforms and validations, as well as a main.rb driver file which reads the data, preprocesses it via Patient, and saves the output and report.

---
+++ main.rb
@@ -0,0 +1,58 @@
+require 'csv'
+require_relative 'patient'
+
+def read_data(fname)
+  data = CSV.parse(
+    File.read(fname),
+    headers: true,
+    header_converters: lambda { |h| h.strip.gsub(/[\u200B-\u200D\uFEFF]/, '') }
+  )
+  data.map(&:to_h)
+end
+
+def preprocess_data(raw_data)
+  data = []
+  for entry in raw_data do
+    patient = Patient.new(entry)
+    patient.clean
+    patient.data["valid"] = patient.valid?
+    data << patient.data
+  end
+  data
+end
+
+def save_data(data, fname)
+  CSV.open(fname, "w") do |csv|
+    csv << data.first.keys
+    for row in data do
+      csv << row.values
+    end
+  end
+
+end
+
+def generate_and_save_report(data, fname_input, fname_output, fname_report)
+  n_patients = data.size
+  n_valid_patients = data.filter{|patient| patient["valid"]}.size
+  n_invalid_patients = n_patients - n_valid_patients
+
+  File.open(fname_report, "w") do |file|
+    file.puts "Data pre-processed successfully at " + Time.now.strftime("%d/%m/%Y %H:%M")
+    file.puts "Input file: #{fname_input}"
+    file.puts "Output file: #{fname_output}"
+    file.puts "Report file: #{fname_output}"
+    file.puts "Total patients: #{n_patients}"
+    file.puts "Valid patients: #{n_valid_patients}"
+    file.puts "Invalid patients: #{n_invalid_patients}"
+  end
+end
+
+
+fname_input = "input.csv"
+fname_output = "output.csv"
+fname_report = "report.txt"
+
+raw_data = read_data(fname_input)
+data = preprocess_data(raw_data)
+save_data(data, fname_output)
+generate_and_save_report(data, fname_input, fname_output, fname_report)
---
+++ patient.rb
@@ -0,0 +1,95 @@
+class Patient
+  attr_accessor :raw_data, :data
+
+  def initialize(raw_data)
+    @raw_data = raw_data
+    @data = raw_data
+  end
+
+  def clean
+    trim_whitespace
+    transform_phone_number
+    transform_all_date_fields
+  end
+
+  def valid?
+    validate_required_fields &&
+      validate_phone_number
+  end
+
+  ### TRANSFORMS
+
+  def trim_whitespace
+    data.each do |k, v|
+      data[k] = v && v.strip
+    end
+  end
+
+  def transform_phone_number
+    # Convert to E.164 format
+    phone = data["phone_number"]
+    return unless phone
+
+    phone = phone.gsub(/[^0-9]/i, "")
+    if phone.length == 10
+      phone = "+1" + phone
+    elsif phone.length == 11 && phone[0] == "1"
+      phone = "+" + phone
+    end
+    data["phone_number"] = phone
+  end
+
+  DATE_FORMATS = {
+    "%m/%d/%y" => /\A\d{1,2}\/\d{1,2}\/\d{1,2}\z/,
+    "%m/%d/%Y" => /\A\d{1,2}\/\d{1,2}\/\d{4}\z/,
+    "%m-%d-%y" => /\A\d{1,2}-\d{1,2}-\d{1,2}\z/,
+    "%m-%d-%Y" => /\A\d{1,2}-\d{1,2}-\d{4}\z/,
+    "%Y-%m-%d" => /\A\d{4}-\d{1,2}-\d{1,2}\z/
+  }
+
+  def transform_date(date)
+    # Convert to ISO8601 format (YYYY-MM-DD)
+    return unless date
+
+    parsed = nil
+    DATE_FORMATS.each do |format, regex|
+      begin
+        if date.match(regex)
+          # NOTE: We first do a regex match here to address the following default Date.strptime behavior:
+          # Date.strptime("01/02/1920", "%m/%d/%y") is incorrectly parsed as "2019-01-02", instead of recognizing this is
+          # not a 2-digit year, and moving on to the next format to correctly parse it as "1920-01-02"
+          # Similarly, Date.strptime("01/02/19", "%m/%d/%Y") is incorrectly parsed as "0019-01-02", instead of recognizing
+          # this is not a 4-digit year, and using the alternative format to correctly parse it as "2019-01-02"
+          # TODO: Revisit and see if there's a more elegant solution.
+          parsed = Date.strptime(date, format)
+          break
+        end
+      rescue ArgumentError
+      end
+    end
+
+    parsed.iso8601
+  end
+
+  DATE_FIELDS = ["dob", "effective_date", "expiry_date"]
+
+  def transform_all_date_fields
+    DATE_FIELDS.each do |field|
+      data[field] = transform_date(data[field])
+    end
+  end
+
+  # VALIDATIONS
+
+  REQUIRED_FIELDS = ["first_name", "last_name", "dob", "member_id", "effective_date"]
+
+  def validate_required_fields
+    !data.values_at(*REQUIRED_FIELDS).include?(nil)
+  end
+
+  def validate_phone_number
+    phone = data["phone_number"]
+    phone.nil? || (phone.length == 12 && phone[0..1] == "+1" && !phone[2..].match(/\d{10}/).nil?)
+  end
+
+end
